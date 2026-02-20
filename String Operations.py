d4b6a99c45f1ea3839c718e950d7538a5aeb8052
viswa3211-pixel <viswa3211@gmail.com>
2026-02-20 20:22:09 +0530

    Add files via upload

---
+++ String Operations.py
@@ -0,0 +1,13 @@
+first_name = "Hasmitha"
+last_name = "Vaisnavi"
+full_name = first_name + " " + last_name
+example = "Hello"*5
+print("First Name: " , first_name)
+print("Last Name: " , last_name)
+print("Full Name: " , full_name)
+print("String Multiplied 5 times given this result: " , example)
+word = "Coding"
+print("The length of String is: " , len(word))
+print("The first Letter of  String is: " , word[0])
+print("The last Letter of  String is: " , word[5])
+print("String Sliced :", word[0:4])
