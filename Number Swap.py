d28b1351c9b7864309d99b30319fe92d4d27d672
viswa3211-pixel <viswa3211@gmail.com>
2026-02-20 20:03:15 +0530

    Add files via upload

---
+++ Number Swap.py
@@ -0,0 +1,10 @@
+# Take input values from user
+x = input("Enter Value of x: ")
+y = input("Enter Value of y: ")
+#Swapping
+temp = x
+x = y
+y = temp
+# Displaying results after swapping
+print("Value of x after swapping: ", x)
+print("Value of y after swapping: ", y)
