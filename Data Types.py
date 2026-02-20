878b6a0a4434430b8b8ade97b96ac6bc8818095e
viswa3211-pixel <viswa3211@gmail.com>
2026-02-20 19:30:20 +0530

    Add files via upload

---
+++ Data Types.py
@@ -0,0 +1,19 @@
+name = "Hasmitha"
+age = 15
+is_student = True
+weight = 38.5
+print("Name:", name)
+print("Data Type of name:", type(name))
+print("Age:", age)
+print("Data Type of age:", type(age))
+print("is_student:", is_student)
+print("Data Type of is_student:", type(is_student))
+print("Weight:", weight)
+print("Data Type of weight:", type(weight))
+print("\n After Type Casting.....")
+age = str(age)
+print(age)
+print("Data Type of age after type casting:", type(age))
+weight = int(weight)
+print(weight)
+print("Data Type of weight after type casting:", type(weight))
