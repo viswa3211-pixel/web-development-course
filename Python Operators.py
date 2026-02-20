5210ab731d940eeb87388b0cd278c44eb802e3a5
viswa3211-pixel <viswa3211@gmail.com>
2026-02-20 19:50:10 +0530

    Add files via upload

---
+++ Python Operators.py
@@ -0,0 +1,19 @@
+num1 = 45
+num2 = 3
+
+print("Number 1",num1)
+print("Number 2",num2)
+print("Addition :", num1 + num2)
+print("Difference :", num1 - num2)
+print("Product :", num1 * num2  )
+print("Division :", num1 / num2 )
+print("Floor division :", num1 // num2 )
+print("Modulus Operation :", num1 % num2 )
+print("Square :", num2**2 )
+print("Square root :", num1**0.5 )
+print("Equal ?", num1 == num2 )
+print("Number 1 Greater?", num1 > num2 )
+print("Number 2 Greater?", num1 < num2 )
+print("Not Equal ?", num1 != num2 )
+result = num1/2 + num2**2+10
+print("Result of the given equation is :", result)
