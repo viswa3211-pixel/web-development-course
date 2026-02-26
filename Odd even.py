e6c8aad84f428dc0ab6c1fa8374c9f1081e0aa74
viswa3211-pixel <viswa3211@gmail.com>
2026-02-26 19:20:04 +0530

    Add files via upload

---
+++ Odd even.py
@@ -0,0 +1,6 @@
+number=int(input("Enter a number to check: "))
+print("Number need to be checked :",number)
+if number%2==0:
+    print("The is an even number")
+else:
+    print("The is an odd number")
