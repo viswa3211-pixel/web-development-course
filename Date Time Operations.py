0aa372fac7beb8b72ceb8239276d0dfa578bde0a
viswa3211-pixel <viswa3211@gmail.com>
2026-02-26 19:52:31 +0530

    Add files via upload

---
+++ Date Time Operations.py
@@ -0,0 +1,11 @@
+import datetime
+
+#using now() to get current time
+current_time = datetime.datetime.now()
+
+#Printing value of now.
+print("Time now at greenwich meridian is:",end=" ")
+print(current_time)
+#print calender of the year 2022
+import calendar
+print("\n", calendar.calendar(2022))
