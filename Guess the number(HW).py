b4b7c3180a41981038d3aa2d5f5c930b8fecbb96
viswa3211-pixel <viswa3211@gmail.com>
2026-03-19 16:50:16 +0530

    Add files via upload

--- Guess the number(HW).py
+++ Guess the number(HW).py
@@ -1,10 +1,9 @@
 import random
 
-my_name = input("Please enter your name: ")
+print("Enter your name:- ")
+name = input()
 
-# Computer selects a random number
 number = random.randint(1, 100)
-
 attempts = 5
 
 print("Welcome to the Number Guessing Game!")
@@ -14,16 +13,28 @@ print("You only have 5 attempts.")
 for i in range(attempts):
     guess = int(input("Enter your guess: "))
 
+    # Check if correct
     if guess == number:
-        print("🎉 Congratulations!" + my_name + "! You guessed the number.")
-        print("You guessed it in", i+1, "attempts.")
+        print("🎉 Congratulations!" + name + ", you guessed the number.")
+        print("You guessed it in", i + 1, "attempts.")
         break
-    elif guess < number:
+
+    # Check how close the guess is
+    difference = abs(number - guess)
+
+    if difference <= 5:
+        print("🔥 Very close!")
+    elif difference <= 10:
+        print("👍 Close!")
+
+    # High / Low hint
+    if guess < number:
         print("Too low!")
     else:
         print("Too high!")
 
-    print("Attempts left:", attempts - (i+1))
+    print("Attempts left:", attempts - (i + 1))
 
+# If user fails all attempts
 if guess != number:
-    print("Better luck next time! "+ my_name + " ❌ Game Over! The correct number was:", number)
+    print("❌ Game Over! " + name + ", the correct number was:", number)
