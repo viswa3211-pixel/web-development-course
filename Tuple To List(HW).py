aafa7e17b5017231eb053f71d0b019652ccf84fb
viswa3211-pixel <viswa3211@gmail.com>
2026-03-20 20:14:03 +0530

    Add files via upload

---
+++ Tuple To List(HW).py
@@ -0,0 +1,9 @@
+# Define a tuple
+my_tuple = (1, 2, 3, 4, 5)
+
+# Convert tuple to list
+my_list = list(my_tuple)
+
+# Display results
+print("Original Tuple:", my_tuple)
+print("Converted List:", my_list)
